dcfba61cef9f26e7b0487ff02649c39244d678f1
zjjxj <592670776@qq.com>
2016-04-26 12:19:38 +0800

    jasmine v0.1

--- pos_v0.1/test/main-test.js
+++ pos_v0.1/test/main-test.js
@@ -55,6 +55,157 @@ describe('pos', function() {
     ];
   });
 
+  describe('Unit Test', function() {
+    describe('Text Items',function () {
+      it('return right Items',function () {
+        var Items=buildItems(inputs);
+        expect(Items).toEqual([
+          {
+            barcode: 'ITEM000000',
+            name: '可口可乐',
+            unit: '瓶',
+            price: 3.00,
+            count:5
+
+          },
+          {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00,
+            count:2
+
+          },
+          {
+            barcode: 'ITEM000004',
+            name: '电池',
+            unit: '个',
+            price: 2.00,
+            count:1
+          }
+
+        ]);
+
+      });
+
+    });
+  });
+
+  describe('Unit Test', function() {
+    describe('Text cartItems',function () {
+      it('return right Items',function () {
+        var Items=buildItems(inputs);
+        var cartItems=buildCartItems(Items);
+        expect(cartItems).toEqual([
+          {
+            Item:{
+              barcode: 'ITEM000000',
+              name: '可口可乐',
+              unit: '瓶',
+              price: 3.00,
+              count:5
+
+            },
+
+            subTotal:15
+
+          },
+          {
+            Item:{
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00,
+              count:2
+
+            },
+
+            subTotal:6
+
+          },
+          {
+            Item:{
+              barcode: 'ITEM000004',
+              name: '电池',
+              unit: '个',
+              price: 2.00,
+              count:1
+
+            },
+
+            subTotal:2
+
+          }
+
+
+        ]);
+
+      });
+
+    });
+  });
+
+  describe('Unit Test', function() {
+    describe('Text receiptItems',function () {
+      it('return right receiptItems',function () {
+        var Items=buildItems(inputs);
+        var cartItems=buildCartItems(Items);
+        var  receiptItems= buildReceiptItems(cartItems);
+        expect(receiptItems).toEqual({
+
+          cartItems:[{
+            Item:{
+              barcode: 'ITEM000000',
+              name: '可口可乐',
+              unit: '瓶',
+              price: 3.00,
+              count:5
+
+            },
+
+            subTotal:15
+
+          },
+            {
+              Item:{
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count:2
+              },
+
+              subTotal:6
+
+            },
+            {
+              Item:{
+                barcode: 'ITEM000004',
+                name: '电池',
+                unit: '个',
+                price: 2.00,
+                count:1
+
+              },
+
+              subTotal:2
+
+            }],
+          total:23
+
+
+        });
+
+      });
+
+    });
+  });
+
+
+
+
+
+
   it('should print correct text', function() {
 
     spyOn(console, 'log');
